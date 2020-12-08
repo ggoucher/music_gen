8a500cc027e22be016145678b6c6c257a1278dee
Gerard Goucher <ggoucher@bowdoin.edu>
2020-12-07 21:29:22 -0500

    Better documents code

--- relax_alt.rb
+++ relax_alt.rb
@@ -11,8 +11,11 @@ fmaj = chord(:F4, :major)
 time = 0.6
 
 use_bpm tempo
+#pattern upward to introduce song
 play_pattern_timed emaj, 1, attack: 2, release: 6
-2.times do
+#our main musical loop
+3.times do
+  #the primary loop
   2.times do
     play invert_chord(chord(:E3, :major), 1), attack: 2, release: 6
     sleep(5)
@@ -20,11 +23,6 @@ play_pattern_timed emaj, 1, attack: 2, release: 6
     sleep(5)
     play invert_chord(emaj, 1), attack: 2, release: 6
     sleep(5)
-    ##| play chord(:E3, :major), attack: 2, release: 6
-    ##| sleep(5)
-    ##| play invert_chord(chord(:E3, :major), 1), attack: 2, release: 6
-    sleep(5)
-    tempo -= 1
     
     play invert_chord(chord(:D3, :major), 1), attack: 2, release: 6
     sleep(5)
@@ -32,32 +30,24 @@ play_pattern_timed emaj, 1, attack: 2, release: 6
     sleep(5)
     play invert_chord(dmaj, 1), attack: 2, release: 6
     sleep(5)
-    ##| play chord(:D3, :major), attack: 2, release: 6
-    ##| sleep(5)
-    ##| play invert_chord(chord(:D3, :major), 1), attack: 2, release: 6
-    sleep(5)
   end
   
   #sequence up for surprise
+  play chord(:E3, :major), attack: 2, release: 6
+  sleep(5)
   play emaj, attack: 2, release: 6
   sleep(5)
   play invert_chord(emaj, 1), attack: 2, release: 6
   sleep(5)
-  play chord(:E3, :major), attack: 2, release: 6
-  sleep(5)
-  play invert_chord(chord(:E3, :major), 1), attack: 2, release: 6
-  sleep(5)
-  ##| tempo -= 1
   
+  play chord(:F3, :major), attack: 2, release: 6
+  sleep(5)
   play fmaj, attack: 2, release: 6
   sleep(5)
   play invert_chord(fmaj, 1), attack: 2, release: 6
   sleep(5)
-  play chord(:F3, :major), attack: 2, release: 6
-  sleep(5)
-  play invert_chord(chord(:F3, :major), 1), attack: 2, release: 6
-  sleep(5)
 end
+#inverse pattern for the end of the song 
 play_pattern_timed invert_chord(emaj, 1), 1, attack: 2, release: 8
 
 
